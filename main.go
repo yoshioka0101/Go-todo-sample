a18228aabb08fd0db243b9ba95a451f070442eda
yoshioka0101 <yuuki0205nano@gmail.com>
2026-05-10 16:37:42 +0900

    feat: ginの導入とhealthcheck

--- main.go
+++ main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	r := gin.Default()
+
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "status OK",
+		})
+	})
+	r.Run(":8080")
 }
